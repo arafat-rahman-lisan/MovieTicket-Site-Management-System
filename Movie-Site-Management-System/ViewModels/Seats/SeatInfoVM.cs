bd1bdcab31ed0d99d5255e2199f3e2346f190f98
Arafat Rahman Lisan <arafatrahmanlisan@gmail.com>
2025-09-18 00:02:32 +0600

    Add files via upload

---
+++ Movie-Site-Management-System/ViewModels/Seats/SeatInfoVM.cs
@@ -0,0 +1,11 @@
+﻿using Movie_Site_Management_System.Data.Enums;
+
+public class SeatInfoVM
+{
+    public long SeatId { get; set; }
+    public string RowLabel { get; set; } = string.Empty;
+    public int SeatNumber { get; set; }
+    public short SeatTypeId { get; set; }
+    public ShowSeatStatus Status { get; set; }
+    public decimal Price { get; set; }
+}
--- Movie-Site-Management-System/ViewModels/Seats/SeatVM.cs
+++ Movie-Site-Management-System/ViewModels/Seats/SeatVM.cs
@@ -1,1 +1,15 @@
-
+﻿namespace Movie_Site_Management_System.ViewModels.Seats
+{
+    public class SeatVM
+    {
+        public long ShowSeatId { get; set; }
+        public long SeatId { get; set; }
+        public string Label { get; set; } = "";
+        public decimal Price { get; set; }
+        public short SeatTypeId { get; set; }
+        public string SeatType { get; set; } = "";
+        public bool IsDisabled { get; set; }
+        public bool IsBooked { get; set; }
+        public bool IsHeld { get; set; }
+    }
+}
