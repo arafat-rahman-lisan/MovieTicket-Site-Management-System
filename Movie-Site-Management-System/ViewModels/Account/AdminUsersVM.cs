d613b77857c71cd5d0488b7c28c57437f3f130ee
Arafat Rahman Lisan <arafatrahmanlisan@gmail.com>
2025-09-17 23:45:59 +0600

    Add files via upload

---
+++ Movie-Site-Management-System/ViewModels/Account/AdminUsersVM.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Movie_Site_Management_System.ViewModels.Account
+{
+    public class AdminUsersVM
+    {
+        public List<AdminUserRow> Users { get; set; } = new();
+    }
+}
---
+++ Movie-Site-Management-System/ViewModels/Account/UsersVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Movie_Site_Management_System.ViewModels.Account
+{
+    public class UsersVM
+    {
+        public List<UserRow> Users { get; set; } = new();
+    }
+
+}
