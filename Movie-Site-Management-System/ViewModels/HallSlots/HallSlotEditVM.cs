e4f2000c961edc567eaeb7a64fd5465a747ae20e
Arafat Rahman Lisan <arafatrahmanlisan@gmail.com>
2025-09-18 00:00:04 +0600

    Add files via upload

--- Movie-Site-Management-System/ViewModels/HallSlots/HallSlotEditVM.cs
+++ Movie-Site-Management-System/ViewModels/HallSlots/HallSlotEditVM.cs
@@ -1,1 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace Movie_Site_Management_System.ViewModels.HallSlots
+{
+    public class HallSlotEditVM
+    {
+        public long? HallSlotId { get; set; }
+
+        [Required]
+        [Display(Name = "Hall")]
+        public long HallId { get; set; }
+
+        [Required, Range(1, 50, ErrorMessage = "Slot number must be 1-50.")]
+        [Display(Name = "Slot #")]
+        public int SlotNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Start")]
+        public TimeOnly StartTime { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "End")]
+        public TimeOnly EndTime { get; set; }
+
+        [Display(Name = "Active")]
+        public bool IsActive { get; set; }
+    }
+}
