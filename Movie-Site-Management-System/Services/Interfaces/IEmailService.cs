a252753c82ba1b37d41a73ce8e90a791b27d0201
Arafat Rahman Lisan <arafatrahmanlisan@gmail.com>
2025-09-26 02:53:23 +0600

    Add files via upload

---
+++ Movie-Site-Management-System/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿namespace Movie_Site_Management_System.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendAsync(
+            string toEmail,
+            string subject,
+            string htmlBody,
+            byte[]? attachmentBytes = null,
+            string? attachmentName = null,
+            string? mimeType = null,
+            CancellationToken ct = default);
+    }
+}
